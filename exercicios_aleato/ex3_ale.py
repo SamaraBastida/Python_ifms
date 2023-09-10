7b3ffc8eafca18b5928b42ee2b862977d54ae544
SamaraBastida <samara.venancio08@gmail.com>
2023-09-10 18:44:02 -0400

    commit_4

--- exercicios_aleato/ex3_ale.py
+++ exercicios_aleato/ex3_ale.py
@@ -1,1 +1,16 @@
 #Dado um inteiro n, gerar três inteiros aleatórios entre −n e n
+
+import random
+num = int (input("insira um num inteiro "))
+
+primeiro = num * (-1) 
+ultimo = num
+quantidade = ultimo - primeiro + 1
+sorteado = primeiro + int(random.random() * quantidade ) 
+sorteado2 = primeiro + int(random.random() * quantidade ) 
+sorteado3 = primeiro + int(random.random() * quantidade ) 
+
+
+print("Primeiro inteiro aleatorio:",sorteado)
+print("\nSegundo inteiro aleatorio:",sorteado2)
+print("\nTerceiro inteiro aleatorio:",sorteado3)
--- exercicios_aleato/ex4_ale.py
+++ exercicios_aleato/ex4_ale.py
@@ -1,1 +1,16 @@
 #Dados dois inteiros m e n gerar três inteiros aleatórios entre m e n.
+import random
+
+n = int(input("insira o valor de n: "))
+m = int(input("insira o valor de m: "))
+
+primeiro = n
+ultimo = m
+quantidade = ultimo - primeiro + 1
+sorteado = primeiro + int(random.random() * quantidade)
+sorteado2 = primeiro + int(random.random() * quantidade)+1
+sorteado3 = primeiro + int(random.random() * quantidade)
+
+print("Primeiro numero aleatorio", sorteado)
+print("\nSegundo numero aleatorio", sorteado2)
+print("\nTerceiro numero aleatorio", sorteado3)
