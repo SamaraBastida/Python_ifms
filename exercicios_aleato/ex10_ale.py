aeedf56ce1cf80d1b2619f6fd51ae92477d7a94d
SamaraBastida <samara.venancio08@gmail.com>
2023-09-10 22:10:53 -0400

    commit_6

--- exercicios_aleato/ex10_ale.py
+++ exercicios_aleato/ex10_ale.py
@@ -2,3 +2,27 @@
 # uma letra maiúscula se o usuário inseriu 1, uma letra minúscula 
 # se o usuário inseriu 2 e um dígito se o usuário inseriu qualquer valor
 # que não seja 1 ou 2.
+
+import random
+n = int(input("Digite 1 para MAIUSCULA. ou 2 minuscula: "))
+
+if (n == 1):
+    
+    primeiro = 65
+    ultimo = 90
+    quantidade = ultimo - primeiro + 1
+    maiuscula = primeiro + int(random.random() * quantidade)
+    print(chr(maiuscula))
+    
+elif(n == 2):
+    p = 97
+    u = 122
+    q = u - p + 1
+    minuscula = p + int(random.random() * q)
+    print(chr(minuscula))
+else:
+    pn = 48
+    un = 57
+    qn = un - pn +1
+    inteiro = pn + int(random.random()*qn)
+    print(inteiro)
--- exercicios_aleato/ex8_ale.py
+++ exercicios_aleato/ex8_ale.py
@@ -1,1 +1,22 @@
 #Gerar uma senha aleatória de 8 dígitos com letras maiúsculas, minúsculas e inteiros
+import random
+
+primeiro = 65
+ultimo = 90
+quantidade = ultimo - primeiro + 1
+maiuscula = primeiro + int(random.random() * quantidade)
+
+p = 97
+u = 122
+q = u - p + 1
+minuscula = p + int(random.random() * q)
+
+pn = 48
+un = 57
+qn = un - pn +1
+inteiro = pn + int(random.random()*qn)
+
+
+
+#senha?????
+#print("Sua senha é:",senha )
